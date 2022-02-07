afc7f57218c634c25525d856d3326f654f35bbde
Leslie Werner <lesliewerner@Leslies-MBP.lan>
2022-02-07 17:18:48 -0500

    edited

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -61,10 +61,9 @@ CREATE TABLE titles (
 
 SELECT * FROM departments;
 
-
 SELECT * FROM managers;
 
-DROP TABLE retirement_info;
+DROP TABLE managers;
 
 -- Retirement eligibility
 SELECT COUNT(first_name)
@@ -72,68 +71,49 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+SELECT * FROM retirement_info;
+
 SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- 1. Retrieve the emp_no, first_name, and last_name columns from the Employees table.
-SELECT emp_no, first_name, last_name 
-FROM employees
--- 2. Retrieve the title, from_date, and to_date columns from the Titles table.
-SELECT title, from_date, to_date 
-FROM titles
+-- Create a new table using the INTO clause for retirement_titles.
 
--- 3. Create a new table using the INTO clause.
-SELECT title, from_date, to_date, emp_no
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
 INTO retirement_titles
-FROM titles;
+FROM employees AS e
+JOIN titles AS ti ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 
-DROP TABLE retirement_titles;
-
-SELECT * FROM retirement_emp;
-
-SELECT emp_no, first_name, last_name 
-INTO retirement_emp
-FROM employees;
-
-SELECT retirement_titles.emp_no, first_name, last_name, title, from_date, to_date
-INTO emp_info_tab
-FROM retirement_titles
-JOIN retirement_info ON retirement_titles.emp_no = retirement_info.emp_no;
-
-SELECT * FROM emp_info_tab;
+SELECT * FROM retirement_titles;
 
 -- UNIQUE TITLES.CSV INFO 
 
 SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
-INTO emp_retirement_info
+INTO unique_titles
 FROM emp_info_tab
 WHERE (to_date BETWEEN '1920-01-01' AND '9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
-DROP TABLE emp_retirement_info; 
+DROP TABLE unique_titles; 
 
-SELECT * FROM emp_retirement_info;
+SELECT * FROM unique_titles;
 
 
 -- Retrieve the number of employees by most recent job title who are about to retire
 
 SELECT COUNT(title), title
-FROM emp_retirement_info
-GROUP BY title;
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
 
 -- Del. 2 Write a query to create Mentorship Elgibility table that holds the employees wh oare eligible to participate in mentorship program.
-SELECT emp_no, first_name, last_name, birth_date
-FROM employees;
-
-SELECT from_date, to_date
-FROM dept_emp;
-
-SELECT title
-FROM titles;
-
 -- USE DISTINCT ON to find elgibility
 
 SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, d.from_date, d.to_date, title
@@ -142,6 +122,7 @@ FROM employees AS e
 JOIN dept_emp AS d ON e.emp_no = d.emp_no
 JOIN titles ON d.emp_no = titles.emp_no
 WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date BETWEEN '1920-01-01' AND '9999-01-01')
 ORDER BY emp_no;
 
 SELECT * FROM mentorship_elgibility;
